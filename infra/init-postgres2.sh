e1146e40f134c207dda4e007ad1b2bf4255b1ad8
nkitan <ankitdas2k@gmail.com>
2025-01-22 01:23:47 +0530

    Infra | Added PGAdmin, Fixed Replication Issues

--- infra/init-postgres2.sh
+++ infra/init-postgres2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+. ./.env
 set -e
 
 # Create a subscription to postgres1
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE SUBSCRIPTION app_subscription
-  CONNECTION 'host=postgres1 port=5432 user=replicator password=replicator_password dbname=app_db'
-  PUBLICATION app_publication;
+  CREATE SUBSCRIPTION $POSTGRES_SUBSCRIPTION_NAME
+  CONNECTION 'host=postgres1 port=5432 user=$REPLICATOR_USER password=$REPLICATOR_PASSWORD dbname=$POSTGRES_DB'
+  PUBLICATION $POSTGRES_PUBLICATION_NAME;
 EOSQL
